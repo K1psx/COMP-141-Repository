1cc926f6d3e1722e4d40b575d0aac87c5a742d95
K1psx <150890884+K1psx@users.noreply.github.com>
2024-08-28 08:13:33 -0500

    New PRGM

---
+++ PerpetualCalendar.java
@@ -0,0 +1,41 @@
+public class PerpetualCalendar {
+    public static void main(String[] args) {
+        int daysInMonth = 31;
+        int firstSunday = 6;
+        printCalendar(daysInMonth, firstSunday);
+    }
+
+    public static void printCalendar(int daysInMonth, int firstSunday) {
+        // Print the header
+        System.out.println("  Sun  Mon  Tue  Wed  Thu  Fri  Sat");
+        System.out.println("+------+------+------+------+------+------+------+");
+
+        for (int i = 1; i < firstSunday; i++) {
+            System.out.print("|" + padded(0, 4));
+        }
+
+        for (int day = 1; day <= daysInMonth; day++) {
+            System.out.print("|" + padded(day, 4));
+
+            if ((day + firstSunday - 1) % 7 == 0) {
+                System.out.println();
+            }
+        }
+
+        int daysOnLastLine = (daysInMonth + firstSunday - 1) % 7;
+        for (int i = daysOnLastLine; i < 7 && i != 0; i++) {
+            System.out.print("|" + padded(0, 4));
+        }
+        System.out.println("|");
+
+        System.out.println("+------+------+------+------+------+------+------+");
+    }
+
+    public static String padded(int n, int width) {
+        String s = "" + n;
+        for (int i = s.length(); i < width; i++) {
+            s = " " + s;
+        }
+        return s;
+    }
+}
